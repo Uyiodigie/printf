bd4196323b2007056a2845fae3c9c1f63274ee5c
UyiOdigie <odigieuyioghosa256@gmail.com>
2022-07-15 11:06:42 -0700

    edit documents

--- _printf.c
+++ _printf.c
@@ -9,40 +9,41 @@
  */
 int _printf(const char *format, ...)
 {
-  int count = 0, i = 0;
-  va_list data;
-  va_start(data, format);
+	int count = 0, i = 0;
+	va_list data;
 
-  for (i = 0; format[i] != '\0'; )
-  {
-    if (format[i] != '%')
-    {
-      count += _putchar(format[i]);
-      i++;
-    }
-    else if (format[i] == '%' && format[i + 1] != ' ')
-    { 
-      switch (format[i + 1])
-      {
-        case 'c':
-          count += _putchar(va_arg(data, int));
-          break;
-        case 's':
-          count += print_string(va_arg(data, char *));
-          break;
-        case '%':
-          count += _putchar('%');
-          break;
-        case 'd':
-          count += _print_decimal(va_arg(data, int));
-          break
-        default:
-          break;
-      }
-      i += 2;
-    }
-  }
-  var_end(data)
-  return (count);
+	va_start(data, format);
+
+	for (i = 0; format[i] != '\0'; )
+	{
+		if (format[i] != '%')
+		{
+			count += _putchar(format[i]);
+			i++;
+		}
+		else if (format[i] == '%' && format[i + 1] != ' ')
+		{
+			switch (format[i + 1])
+			{
+				case 'c':
+					count += _putchar(va_arg(data, int));
+					break;
+				case 's':
+					count += print_string(va_arg(data, char *));
+					break;
+				case '%':
+					count += _putchar('%');
+					break;
+				case 'd':
+					count += _print_decimal(va_arg(data, int));
+					break;
+				default:
+					break;
+			}
+			i += 2;
+		}
+	}
+	var_end(data)
+	return (count);
 }
 
--- print_string.c
+++ print_string.c
@@ -7,8 +7,9 @@
  */
 int print_string(char *string)
 {
-  int i, count = 0;
-  for (i = 0; string[i] != '\0'; i++)
-    count += _putchar(string[i]);
-  return (count);
+	int i, count = 0;
+
+	for (i = 0; string[i] != '\0'; i++)
+		count += _putchar(string[i]);
+	return (count);
 }
